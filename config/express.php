cfff080de7686753d62cdafcab0a4e8d6b958d6a
caojiayuan <cjy632258@hotmail.com>
2019-04-18 16:34:21 +0800

    comment

--- config/express.php
+++ config/express.php
@@ -12,6 +12,8 @@ return [
     ],
 
     'ali' => [
+        // Default uri
+        // https://market.aliyun.com/products/56928004/cmapi021863.html?spm=5176.2020520132.101.2.7cd37218n1PP72#sku=yuncode1586300000
         'baseUri' => env('ALI_APP_BASE_URI', 'https://wuliu.market.alicloudapi.com/kdi'),
         'appCode' => env('ALI_APP_CODE'),
     ]
